6870674c95f5bf2d36d7a32187f11e4175616a9d
admin <13633779759@163.com>
2022-10-12 21:46:15 +0800

    记录一下

--- pandas_study/hello_pandas.py
+++ pandas_study/hello_pandas.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import numpy as np
+import string
 
 data = np.array(['a', 'b', 'c'])
 s = pd.Series(data)
 print(s)
+
+se = pd.Series([1, 2, 3, 4], list("abcd"))
+print(se)
+temp_dict = {"name": "jx", "age": 18, "tel": 13}
+t = pd.Series(temp_dict)
+print(t)
+print(t['name'])
